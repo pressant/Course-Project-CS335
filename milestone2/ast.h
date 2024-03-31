4cba4d76787a4fb9f3df190d359cd8eaa38576a1
Lakshvant Balachandran <lakshvant@cse.iitk.ac.in>
2024-03-31 11:32:31 +0000

    Upload New File

---
+++ milestone2/ast.h
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "symtab.cpp"
+
+struct Node {
+  
+  int count;
+  SYMTAB* mytab;
+  vector<Param*> p_f;
+  std::string label;
+  std::string category;
+  std::vector<Node*> children;
+  std::string type;
+
+};
+
+// void print_node(Node *n, std::string, int);
+void printNode(std::ostream& out, Node* node);
+
+void printDot(Node* root, const std::string& filename);
+Node *create_node(std::string label);
+Node *create_node(std::string label, std::string val);
+void simplify(Node* node);
