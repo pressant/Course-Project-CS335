36fad4335acd6d3d82139c2d148f9ee313203b80
Lakshvant <95269743+Lakshvant@users.noreply.github.com>
2024-04-03 10:39:35 +0530

    3ac.h added

---
+++ 3ac.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <tuple>
+#include <bitset>
+#include <array>
+#include <deque>
+#include <list>
+#include <functional>
+#include <iterator>
+#include <iomanip>
+#include <memory>
+#include <numeric>
+#include <stdexcept>
+#include <utility>
+#include <forward_list>
+#include "ast.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+typedef struct quadruple{
+    string op;
+    string arg1;
+    string arg2;
+    string result;
+    int index;// target label for this particular instruction
+}quadruple;
+
+void emitt(string,string,string,string,int);
+void print3AC_code();
+string new_temporary();
