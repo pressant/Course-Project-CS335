bf518a7418468a3b8ea47670670483136d72304f
harshmohan <harshmohan@cse.iitk.ac.in>
2024-04-18 23:32:02 +0530

    final

--- milestone3/milestone3/src/test.py
+++
@@ -1,7 +0,0 @@
-def sum(x:int , y:int)->int:
-    z:int = x + y
-    return z
-
-x:int = 1
-y:int =5
-print(sum(x,y))
--- milestone3/milestone3/tests/test1.py
+++
@@ -1,8 +0,0 @@
-def sum(x:int , y:int)->int:
-    z:int = x + y
-    return z
-def main():
-    print(sum(1,2))
-if __name__ == "__main__":
-    main()
-    
